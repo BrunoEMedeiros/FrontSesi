0c028cde2b35848b3cd15b0e9770bc30d35c03ed
D3adSec <bruno.emedeiros99@gmail.com>
2023-11-17 10:55:24 -0300

    GET,POST,PUT

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Fetch serve para requisicoes HTTP
 
 fetch("http://localhost:3000/produtos")
 .then((resposta)=>{
@@ -9,13 +8,21 @@ fetch("http://localhost:3000/produtos")
             const div = document.querySelector("#root");
             dados.map((produto)=>{
                 const card = document.createElement("article")
+                card.id = produto.id
                 const desc = document.createElement("span")
                 desc.innerText = produto.desc
                 const preco = document.createElement("span")
                 preco.innerText = produto.preco
                 const qtd = document.createElement("span")
                 qtd.innerText = produto.qtd
-                card.append(desc,preco,qtd)
+                const edit = document.createElement("a")
+                edit.className = "edit"
+                edit.innerText = "editar"
+edit.href  = `./formulario.html?id=${produto.id}&desc=${produto.desc}&preco=${produto.preco}&qtd=${produto.qtd}`              
+                const del = document.createElement("a")
+                del.className = "delete"
+                del.innerText = "excluir"
+                card.append(desc,preco,qtd,edit,del)
                 div.append(card)
             })
         })
